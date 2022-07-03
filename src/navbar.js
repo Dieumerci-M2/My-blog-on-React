e7e362aac5931f3033f6e3c72553ed991fc2ca4d
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2022-07-03 21:01:43 +0200

    first commit

---
+++ src/navbar.js
@@ -0,0 +1,16 @@
+const NAvbar = () => {
+    return (  
+        <div className="accueil">
+            <ul>
+                <li>Acceuil</li>
+                <li>Apropos</li>
+            </ul>
+            <ul>
+                <li>Message</li>
+                <li>Help</li>
+            </ul>
+        </div>
+    );
+}
+ 
+export default NAvbar;
